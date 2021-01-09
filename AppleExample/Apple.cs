92e8f88c2f9ef5aa6eabb6a0ec77dd0ee5f7badd
puglisac <alan.c.puglisi@gmail.com>
2021-01-09 11:03:12 -0600

    exercise complete

---
+++ AppleExample/Apple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace AppleExample
+{
+    public class Apple
+    {
+        #region fields
+        float amountLeft;
+        bool organic;
+        #endregion
+
+        #region properties
+        public float AmountLeft
+        {
+            get { return amountLeft; }
+        }
+
+        public bool Organic
+        {
+            get { return organic; }
+        }
+        #endregion
+
+        public void TakeBite(float size)
+        {
+            amountLeft -= size;
+        }
+
+        public Apple()
+        {
+        }
+    }
+}
